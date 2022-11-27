fcec49d240f41bf7cc357925a0f42c462dc4a23a
Rita Bunets <margarita.bunets@gmail.com>
2022-11-27 21:49:28 +0200

    Add Homework10

---
+++ Homework10/GenericArray.cs
@@ -0,0 +1,51 @@
+﻿namespace Homework10
+{
+    public class GenericArray<T> where T : Human
+    {
+        private T[] _array;
+
+        public GenericArray(int size)
+        {
+            _array = new T[size + 1];
+        }
+
+        public int GetAraryLength() => _array.Length;
+
+        public void AddElement(int index, T element)
+        {
+            _array[index] = element;
+        }
+
+        public T GetElement(int index) => _array[index];
+
+        public void RemoveElement(ref T[] _array, int index)
+        {
+            T[] tmp = new T[_array.Length - 1];
+            for (int i = 0; i < index; i++)
+            {
+                tmp[i] = _array[i];
+            }
+            for (int i = index; i < tmp.Length; i++)
+            {
+                tmp[i] = _array[i + 1];
+            }
+            _array = tmp;
+        }
+
+        public static void ToString(GenericArray<T> _array)
+        {
+            if (typeof(T) == typeof(Woman))
+            {
+                Console.WriteLine("There are only woman.");
+            }
+            else if (typeof(T) == typeof(Man))
+            {
+                Console.WriteLine("There are only man.");
+            }
+            for (int i = 0; i < _array.GetAraryLength()-1; i++)
+            {
+                Console.WriteLine($"Human is {_array.GetElement(i).FName} {_array.GetElement(i).LName}");
+            }
+        }
+    }
+}
---
+++ Homework10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Homework10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var woman = new GenericArray<Woman>(2);
+            var man = new GenericArray<Man>(1);
+
+            GenerateElements(2, woman);
+            GenericArray<Woman>.ToString(woman);
+
+            GenerateElements(1, man);
+            GenericArray<Man>.ToString(man);
+        }
+
+        public static void GenerateElements<T>(int numberOfElements, GenericArray<T> generatedElements) where T : Human, new()
+        {
+            string FName;
+            string LName;
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.WriteLine("Set first name:");
+                FName = Console.ReadLine();
+                Console.WriteLine("Set last name:");
+                LName = Console.ReadLine();
+                var human = new T();
+                human.FName = FName;
+                human.LName = LName;
+                if (typeof(T) == typeof(Woman))
+                {
+                    generatedElements.AddElement(i, human);
+                }
+                else if (typeof(T) == typeof(Man))
+                {
+                    generatedElements.AddElement(i, human);
+                }
+            }
+
+        }
+    }
+}
